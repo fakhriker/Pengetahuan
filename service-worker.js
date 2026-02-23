5621c5c880af5c16b2f805a32c970832fdec71cc
Kerz <siraduejembutdawa@gmail.com>
2026-02-23 14:44:40 +0700

    Update service worker caching strategy and cache name

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,52 @@
-const CACHE_NAME = "miniwiki-v1";
+const CACHE_NAME = "miniwiki-cache";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./ski.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json"
+  "/Pengetahuan/",
+  "/Pengetahuan/index.html",
+  "/Pengetahuan/ski.html",
+  "/Pengetahuan/style.css",
+  "/Pengetahuan/script.js",
+  "/Pengetahuan/manifest.json"
 ];
 
+// Install
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// Fetch (Network First Strategy)
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
